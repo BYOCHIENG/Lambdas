ba29f0dfd4f3d5999854670b663dde91be9414a2
Mich Ochieng <michochieng@gmail.com>
2025-04-08 23:21:55 -0700

    Removed mock, now takes in file as a param

--- ses-contact-form/test-local.sh
+++ ses-contact-form/test-local.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+# Check if event file was provided as an argument
+SAM_EVENT=${1:-"sam-events/success-event.json"}
+
 # Build the Go binary for Lambda
 echo "Building Go binary..."
-GOOS=linux GOARCH=amd64 go build -o bootstrap main.go
+GOOS=linux GOARCH=amd64 go build -o bootstrap
 
 # Check if build succeeded
 if [ $? -ne 0 ]; then
@@ -11,9 +14,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Run the Lambda function locally using SAM
-echo "Invoking Lambda function locally using SAM..."
-sam local invoke ContactFormFunction -e sam-events/test-event.json
+# Run the Lambda function locally using SAM with mock SES client
+echo "Invoking Lambda function locally using SAM CLI..."
+echo "Running event file: $SAM_EVENT..."
+sam local invoke ContactFormFunction -e "$SAM_EVENT"
 
 # Clean up
 echo "Cleaning up..."
